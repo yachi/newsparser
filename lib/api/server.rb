c584acff8d54484e1c6f40c1b9617251ef36178a
Yachi Lo <yaachi@gmail.com>
2012-06-04 00:49:11 +0800

    remove debug loggings

--- lib/api/server.rb
+++ lib/api/server.rb
@@ -29,14 +29,10 @@ module Api
     configure do
       enable :cross_origin
       if ENV['MEMCACHE_SERVERS']
-        puts ENV['MEMCACHE_SERVERS']
-        puts ENV['MEMCACHE_USERNAME']
-        puts ENV['MEMCACHE_PASSWORD']
         set :cache, Dalli::Client.new( ENV['MEMCACHE_SERVERS'],
                                        :username => ENV['MEMCACHE_USERNAME'],
                                        :password => ENV['MEMCACHE_PASSWORD'],
                                        :expires_in => 3600 * 24)
-        puts "finished setting memcache settings"
       else
         set :cache, Dalli::Client.new
       end
@@ -52,31 +48,25 @@ module Api
     end
 
     before do
-      puts "start cache key block"
       keys = request.path.split('/')
       keys << ['d', params['d'].to_s]
       @_cache_key = keys.join('/')
-      puts "finished cache key block"
 
-      puts "start get cache block"
       if value = settings.cache.get(@_cache_key)
         logger.info 'got cache with key: ' << @_cache_key
         @_cache_exists = true
         @result = value
       end
-      puts "finished get cache block"
     end
 
     after do
       if response.status == 200
         cache_control :public, :must_revalidate, :max_age => 3600
       end
-      puts "start set cache block"
       if !@_cache_exists and @_cache_key and @result
         logger.info 'setting cache with key: ' << @_cache_key
         settings.cache.set(@_cache_key, @result)
       end
-      puts "finished set cache block"
     end
   end
 end
