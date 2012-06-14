c9071e9f8193702cb97e4744252e8d00b8643862
Yachi Lo <yaachi@gmail.com>
2012-06-14 23:44:16 +0800

    explicitly return result

--- lib/newsparser/apple.rb
+++ lib/newsparser/apple.rb
@@ -113,8 +113,9 @@ module Newsparser
           result[:media] = {:url => video_link} if video_link
           result[:photo] = ''
           result[:title] = content.css('h1').first.text.strip
-          result[:content] = photo << content.css('#masterContent').inner_html.strip
           result[:original_url] = "http://hk.apple.nextmedia.com/#{self.s_id}/art/#{self.date_str}/#{article_id}"
+          result[:content] = photo << content.css('#masterContent').inner_html.strip
+          return result
         end
       end
     end
