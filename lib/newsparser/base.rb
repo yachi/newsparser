decd4639521b14868c80a946373e1c5edf458464
Yachi Lo <yaachi@gmail.com>
2012-06-23 22:43:20 +0800

    default gzip header

--- lib/newsparser/base.rb
+++ lib/newsparser/base.rb
@@ -16,8 +16,9 @@ module Newsparser
 
     def get_url(url, options)
       options[:headers] ||= {}
+      options[:headers]["accept-encoding"] = "deflate, gzip"
       ua = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; HTC-A9191/1.0; zh-cn) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16"
-      options[:headers].merge!({"User-Agent" => ua}){|k, v1, v2| v1}
+      options[:headers].merge!({"User-Agent" => ua}){|k, v1, v2| v1} # reverse_merge!
       response = self.class.get(url, options)
       response.body.to_s
     end
