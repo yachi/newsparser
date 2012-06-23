df83f1c4c1cfd7b652fb4988e7d633d74b60c405
Yachi Lo <yaachi@gmail.com>
2012-06-23 22:02:48 +0800

    not cache sections so frequent

--- lib/api/apple.rb
+++ lib/api/apple.rb
@@ -49,7 +49,6 @@ module Api
           @apple.s_id = params['s'] if params['s'].to_s[/\w+/]
           @apple.s_id ||= 'news'
           @result ||= @apple.article(params[:art])
-          etag @result.hash
           render_result
         end
       end
